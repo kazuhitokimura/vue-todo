967057cfa87fc2c0e81d0a526ff6af6428143b40
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-05-25 23:04:14 +0900

    調整

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about pa-6">
-    <h1>H1テキスト {{ $store.state.appTitle }}</h1>
+    <h1>H1テキスト</h1>
     <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
   </div>
 </template>
