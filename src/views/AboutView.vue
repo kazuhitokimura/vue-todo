ac3c30a577ad7494a4e760832691d0aec8ce1fa4
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-05-29 09:50:13 +0900

    取り急ぎ

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="about pa-6">
+    <div class="about pa-6">
+      <v-row align-contents="center">
+    <v-col>
     <h1>H1テキスト</h1>
     <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-  </div>
+  </v-col>
+    <v-btn>ボタン</v-btn>
+  </v-row>
+  <v-card outlined>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+    </div>
 </template>
+
+<script>
+</script>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="about pa-6">
+    <!--
     <h1>H1テキスト</h1>
     <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+  -->
+  <v-card outlined>
+    <v-card-title>
+      Nutrition
+      <v-spacer></v-spacer>
+      <v-row
+      align-items="center">
+        <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-btn>ボタン</v-btn>
+      </v-row>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
   </div>
 </template>
 
